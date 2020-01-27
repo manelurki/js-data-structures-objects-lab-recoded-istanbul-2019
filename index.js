5e71c6c7658c10c6e5cec8285818d462bf8d605f
manel turki <menelturki@gmail.com>
2020-01-27 17:49:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,11 +14,14 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 
 function deleteFromDriverByKey(driver, key) {
 
-  const newObj = Object.assign({}, driver);
-
-  delete newObj[key];
-
-  return newObj;
+  const obj = { foo: "bar" };
+ 
+const newObj = Object.assign({}, obj);
+ 
+newObj;
+// => { foo: "bar" }
+ 
+delete newObj['foo'];
 }
 
 function destructivelyDeleteFromDriverByKey(driver, key) {
